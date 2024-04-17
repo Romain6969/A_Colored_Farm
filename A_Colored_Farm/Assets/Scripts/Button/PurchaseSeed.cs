1f393ba571252217afdc3c9297987b15e881040c
MeguminFireFox <ambrehardy013@gmail.com>
2024-04-17 15:33:39 +0200

    Dictionnaire Seed

--- A_Colored_Farm/Assets/Scripts/Button/PurchaseSeed.cs
+++ A_Colored_Farm/Assets/Scripts/Button/PurchaseSeed.cs
@@ -15,49 +15,49 @@ public class PurchaseSeed : MonoBehaviour
                 if (_inventoryMain.Money >= _seedMain.GreySeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.GreySeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberGreySeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.GreySeed, 1);
                 }
                 break;
             case 1:
                 if (_inventoryMain.Money >= _seedMain.BlueSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.BlueSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberBlueSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.BlueSeed, 1);
                 }
                 break;
             case 2:
                 if (_inventoryMain.Money >= _seedMain.YellowSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.YellowSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberYellowSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.YellowSeed, 1);
                 }
                 break;
             case 3:
                 if (_inventoryMain.Money >= _seedMain.RedSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.RedSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberRedSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.RedSeed, 1);
                 }
                 break;
             case 4:
                 if (_inventoryMain.Money >= _seedMain.PurpleSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.PurpleSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberPurpleSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.PurpleSeed, 1);
                 }
                 break;
             case 5:
                 if (_inventoryMain.Money >= _seedMain.GreenSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.GreenSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberGreenSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.GreenSeed, 1);
                 }
                 break;
             case 6:
                 if (_inventoryMain.Money >= _seedMain.OrangeSeed.PurchaseValue)
                 {
                     _inventoryMain.Money -= _seedMain.OrangeSeed.PurchaseValue;
-                    _inventoryMain.InventorySeed.NumberOrangeSeed += 1;
+                    _inventoryMain.InventorySeed.AddSeed(_seedMain.OrangeSeed, 1);
                 }
                 break;
         }
--- A_Colored_Farm/Assets/Scripts/Player/Inventory/InventorySeed.cs
+++ A_Colored_Farm/Assets/Scripts/Player/Inventory/InventorySeed.cs
@@ -3,14 +3,6 @@ using UnityEngine;
 
 public class InventorySeed : MonoBehaviour
 {
-    [field: SerializeField] public int NumberGreySeed { get; set; }
-    [field: SerializeField] public int NumberBlueSeed { get; set; }
-    [field: SerializeField] public int NumberYellowSeed { get; set; }
-    [field: SerializeField] public int NumberRedSeed { get; set; }
-    [field: SerializeField] public int NumberPurpleSeed { get; set; }
-    [field: SerializeField] public int NumberGreenSeed { get; set; }
-    [field: SerializeField] public int NumberOrangeSeed { get; set; }
-
     public Dictionary<SeedData, int> Inventory { get; set; } = new();
 
     public void AddSeed(SeedData seed, int amount)
@@ -18,6 +10,7 @@ public class InventorySeed : MonoBehaviour
         if (Inventory.ContainsKey(seed))
         {
             Inventory[seed] += amount;
+            Debug.Log(Inventory[seed]);
         }
         else
         {
