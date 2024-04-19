d94ab17c413e08872076fb60e051c95cbff84941
Romain6969 <romain.braud@e-artsup.net>
2024-04-19 11:08:27 +0100

    Juste un petit push

--- A_Colored_Farm/Assets/Scripts/Colors/PlaceColor.cs
+++ A_Colored_Farm/Assets/Scripts/Colors/PlaceColor.cs
@@ -5,6 +5,7 @@ public class PlaceColor : MonoBehaviour
 {
     [field: SerializeField] public bool IsPainting { get; set; }
     [SerializeField] private PlayerInput _playerInput;
+    private bool _triggered;
 
     public PlayerInput PlayerInput => _playerInput;
 
@@ -22,6 +23,14 @@ public class PlaceColor : MonoBehaviour
         if (context.canceled)
         {
             IsPainting = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Dirt")
+        {
+            _triggered = true;
         }
     }
 }
