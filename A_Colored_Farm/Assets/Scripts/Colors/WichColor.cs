1ce77cc921b145a0ef767a92910ade2ff77ee435
Romain6969 <romain.braud@e-artsup.net>
2024-05-16 13:34:22 +0100

    Premier test avec le Mix

--- A_Colored_Farm/Assets/Scripts/Colors/WichColor.cs
+++ A_Colored_Farm/Assets/Scripts/Colors/WichColor.cs
@@ -1,15 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WichColor : MonoBehaviour
 {
     [field : SerializeField] public int ColorNumber { get; set; }
     [SerializeField] private GameObject _colorPanel;
-    [SerializeField] private Dirt _dirt;
+    [field: SerializeField] public Dirt _dirt { get; set; }
 
     public void OnClickNumberColor(int color)
     {
         ColorNumber = color;
         _colorPanel.SetActive(false);
         _dirt.OnDate();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Dirt")
+        {
+            _dirt = collision.gameObject.GetComponent<Dirt>();
+        }
     }
 }
