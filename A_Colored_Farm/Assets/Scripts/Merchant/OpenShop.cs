b55549136d2cbbd84fcc3906982c0b51df474d15
Romain6969 <romain.braud@e-artsup.net>
2024-05-27 15:50:01 +0100

    rajout de dotween + debut money interactif

--- A_Colored_Farm/Assets/Scripts/Merchant/OpenShop.cs
+++ A_Colored_Farm/Assets/Scripts/Merchant/OpenShop.cs
@@ -37,59 +37,59 @@ public class OpenShop : MonoBehaviour
                         }
                         break;
                     case 1:
-                        _inventoryMain.Money += _seedMain.GreySeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.GreySeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 2:
-                        _inventoryMain.Money += _seedMain.BlueSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.BlueSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 3:
-                        _inventoryMain.Money += _seedMain.YellowSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.YellowSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 4:
-                        _inventoryMain.Money += _seedMain.RedSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.RedSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 5:
-                        _inventoryMain.Money += _seedMain.PurpleSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.PurpleSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 6:
-                        _inventoryMain.Money += _seedMain.GreenSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.GreenSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 7:
-                        _inventoryMain.Money += _seedMain.OrangeSeed.SellValue;
+                        _inventoryMain.AddValue(_seedMain.OrangeSeed.SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 13:
-                        _inventoryMain.Money += _colorMain.ColorsList[0].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[0].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 14:
-                        _inventoryMain.Money += _colorMain.ColorsList[1].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[1].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 15:
-                        _inventoryMain.Money += _colorMain.ColorsList[2].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[2].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 16:
-                        _inventoryMain.Money += _colorMain.ColorsList[3].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[3].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 17:
-                        _inventoryMain.Money += _colorMain.ColorsList[4].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[4].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 18:
-                        _inventoryMain.Money += _colorMain.ColorsList[5].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[5].SellValue);
                         _keepItem.Id = 0;
                         break;
                     case 19:
-                        _inventoryMain.Money += _colorMain.ColorsList[6].SellValue;
+                        _inventoryMain.AddValue(_colorMain.ColorsList[6].SellValue);
                         _keepItem.Id = 0;
                         break;
                 }
