87fd8b0ef5e88e8301baf8ca6c075bcfbeb5f99e
Romain6969 <romain.braud@e-artsup.net>
2024-05-30 14:42:34 +0100

    animation de montrer comment bouger

---
+++ A_Colored_Farm/Assets/Scripts/Visuals/Animation/ShowInputsAnim.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
+
+public class ShowInputsAnim : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _time;
+    [SerializeField] private RebindingDesplayMovementup _movementUp;
+    [SerializeField] private TMP_Text _text;
+
+    private void Start()
+    {
+        _animator.Play("ShowInputsAnim");
+    }
+
+    private void FixedUpdate()
+    {
+        _time += Time.deltaTime;
+
+        if (_time >= 1)
+        {
+            _animator.enabled = false;
+        }
+
+        _text.text = $"Move with : {_movementUp.ActionMovement.action.GetBindingDisplayString(1)} / {_movementUp.ActionMovement.action.GetBindingDisplayString(2)} / {_movementUp.ActionMovement.action.GetBindingDisplayString(3)} / {_movementUp.ActionMovement.action.GetBindingDisplayString(4)}";
+    }
+}
