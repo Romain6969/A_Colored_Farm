29ad24ec2e479517d8bb0109f50992cf85085e60
Romain6969 <romain.braud@e-artsup.net>
2024-06-03 13:29:01 +0100

    Debut Mise en place d'autre couleur

--- A_Colored_Farm/Assets/Scripts/ScriptableObject/Seeds/SeedMain.cs
+++ A_Colored_Farm/Assets/Scripts/ScriptableObject/Seeds/SeedMain.cs
@@ -9,4 +9,10 @@ public class SeedMain : MonoBehaviour
     [field : SerializeField] public SeedData PurpleSeed { get; private set; }
     [field : SerializeField] public SeedData GreenSeed { get; private set; }
     [field: SerializeField] public SeedData OrangeSeed { get; private set; }
+    [field: SerializeField] public SeedData CyanSeed { get; private set; }
+    [field: SerializeField] public SeedData IndigoSeed { get; private set; }
+    [field: SerializeField] public SeedData LimeSeed { get; private set; }
+    [field: SerializeField] public SeedData OcreSeed { get; private set; }
+    [field: SerializeField] public SeedData MagentaSeed { get; private set; }
+    [field: SerializeField] public SeedData OrSeed { get; private set; }
 }
