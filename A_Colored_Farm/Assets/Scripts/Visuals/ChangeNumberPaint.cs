0a0e37ce9cdefbbc7285560e13a59e4e6d884bb4
MeguminFireFox <ambrehardy013@gmail.com>
2024-06-04 18:47:06 +0200

    maj pousse et mixeur animation / debuggage mixeur

--- A_Colored_Farm/Assets/Scripts/Visuals/ChangeNumberPaint.cs
+++ A_Colored_Farm/Assets/Scripts/Visuals/ChangeNumberPaint.cs
@@ -8,12 +8,18 @@ public class ChangeNumberPaint : MonoBehaviour
     [SerializeField] private TMP_Text _bluePaint;
     [SerializeField] private TMP_Text _yellowPaint;
     [SerializeField] private TMP_Text _greyPaint;
+    [SerializeField] private TMP_Text _greenPaint;
+    [SerializeField] private TMP_Text _purplePaint;
+    [SerializeField] private TMP_Text _orangePaint;
 
     private void FixedUpdate()
     {
-        _redPaint.text = $"You have : {_inventoryColor.GetAmount("Red")} paint.";
-        _bluePaint.text = $"You have : {_inventoryColor.GetAmount("Blue")} paint.";
-        _yellowPaint.text = $"You have : {_inventoryColor.GetAmount("Yellow")} paint.";
-        _greyPaint.text = $"You have : {_inventoryColor.GetAmount("Grey")} paint.";
+        _redPaint.text = $"{_inventoryColor.GetAmount("Red")}";
+        _bluePaint.text = $"{_inventoryColor.GetAmount("Blue")}";
+        _yellowPaint.text = $"{_inventoryColor.GetAmount("Yellow")}";
+        _greyPaint.text = $"{_inventoryColor.GetAmount("Grey")}";
+        _greenPaint.text = $"{_inventoryColor.GetAmount("Green")}";
+        _purplePaint.text = $"{_inventoryColor.GetAmount("Purple")}";
+        _orangePaint.text = $"{_inventoryColor.GetAmount("Orange")}";
     }
 }
