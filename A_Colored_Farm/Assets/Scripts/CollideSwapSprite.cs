91ea8da91d947ed64406726e92ce4b263d0bc5a6
MeguminFireFox <ambrehardy013@gmail.com>
2024-06-07 12:47:27 +0200

    Ombre

--- A_Colored_Farm/Assets/Scripts/CollideSwapSprite.cs
+++ A_Colored_Farm/Assets/Scripts/CollideSwapSprite.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class CollideSwapSprite : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private SpriteRenderer _spriteRenderer2;
     [SerializeField] private Sprite _sprite1;
     [SerializeField] private Sprite _sprite2;
+    [SerializeField] private Sprite _sprite3;
+    [SerializeField] private Sprite _sprite4;
     [SerializeField] private Vector3 _position1;
     [SerializeField] private Vector3 _position2;
+    [SerializeField] private Vector3 _position3;
+    [SerializeField] private Vector3 _position4;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,6 +19,8 @@ public class CollideSwapSprite : MonoBehaviour
         {
             _spriteRenderer.sprite = _sprite2;
             transform.position = _position2;
+            _spriteRenderer2.sprite = _sprite4;
+            _spriteRenderer2.transform.position = _position4;
         }
     }
 
@@ -23,6 +30,8 @@ public class CollideSwapSprite : MonoBehaviour
         {
             _spriteRenderer.sprite = _sprite1;
             transform.position = _position1;
+            _spriteRenderer2.sprite = _sprite3;
+            _spriteRenderer2.transform.position = _position3;
         }
     }
 }
