96d2d3c88776f94ab546bd7725b817f61f0008ec
MeguminFireFox <ambrehardy013@gmail.com>
2024-06-07 18:09:12 +0200

    ombre sur les pousses et la moitié des anims du client

--- A_Colored_Farm/Assets/Scripts/Client/ClientMovement.cs
+++ A_Colored_Farm/Assets/Scripts/Client/ClientMovement.cs
@@ -9,6 +9,11 @@ public class ClientMovement : MonoBehaviour
     private float _time = 0;
     [SerializeField] private float _wait = 0;
     private bool _position = false;
+    [SerializeField] public bool Happy { get; set; } = false;
+    [SerializeField] private GameObject _bulle;
+    [SerializeField] private GameObject _explosionBulle;
+    [SerializeField] private GameObject _emotionHappy;
+    [SerializeField] private GameObject _emotionHangry;
     [SerializeField] private Image _image;
     [field : SerializeField] public GameObject ProgressBarGameObject {  get; set; }
     [field : SerializeField] public float Time2 { get; set; }
@@ -24,12 +29,14 @@ public class ClientMovement : MonoBehaviour
         if (_position == false)
         {
             transform.Translate(Vector2.down * _speed * Time.deltaTime);
+            _explosionBulle.SetActive(false);
         }
         else
         {
             ProgressBarGameObject.SetActive(true);
             Time2 += Time.deltaTime;
             _image.fillAmount = Time2 / TimeObjectif;
+            _bulle.SetActive(true);
 
             if (Time2 >= TimeObjectif)
             {
@@ -45,12 +52,25 @@ public class ClientMovement : MonoBehaviour
         {
             Time2 = 0;
             ProgressBarGameObject.SetActive(false);
+            _bulle.SetActive(false);
+            _explosionBulle.SetActive(true);
         }
 
         if (Back == true)
         {
             transform.Translate(Vector2.up * _speed * Time.deltaTime);
             _time += Time.deltaTime;
+
+            if (Happy == true && _time <= 0.15f)
+            {
+                GameObject instantiated = Instantiate(_emotionHappy);
+                instantiated.transform.position = transform.position;
+            }
+            else if (_time <= 0.15f)
+            {
+                GameObject instantiated = Instantiate(_emotionHangry);
+                instantiated.transform.position = transform.position;
+            }
 
             if (_time >= 15)
             {
